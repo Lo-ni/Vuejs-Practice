607367bd74920d3587277918bafd52a0d5ed99cf
Loni <fisher-s@web.de>
2021-09-04 14:31:01 +0200

    Login form, network call, login user, forward to main page on login success

--- space-capsule-app/src/views/Login.vue
+++ space-capsule-app/src/views/Login.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="login">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Login</h1>
+    <h6 v-if="isLoggedIn">
+      {{ loginError }}
+    </h6>
+    <FormulateForm name="loginForm" @submit="loginUser">
+      <FormulateInput
+        name="username"
+        type="text"
+        v-model="input.username"
+        placeholder="Username"
+      />
+      <FormulateInput
+        name="password"
+        type="password"
+        v-model="input.password"
+        placeholder="Password"
+      />
+      <FormulateInput type="submit" label="Login" />
+    </FormulateForm>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: "",
+      },
+      isLoggedIn: false,
+      loginError: "",
+    };
+  },
+  methods: {
+    loginUser() {
+      this.$formulate.handle(
+        {
+          inputErrors: {
+            username:
+              this.input.username.length < 3 ? "Username too short" : "",
+            password:
+              this.input.password.length < 6 ? "Password too short" : "",
+          },
+        },
+        "loginForm"
+      );
+
+      if (this.input.username.length >= 3 && this.input.password.length >= 6) {
+        axios
+          .get("https://randomuser.me/api/")
+          .then((response) => {
+            this.isLoggedIn = response.status === 200;
+            this.loginError = !this.isLoggedIn ? "Login Error" : "";
+            if (this.isLoggedIn) {
+              this.$router.push("main");
+            }
+          })
+          .catch((error) => {
+            this.isLoggedIn = false;
+            this.loginError = error;
+          });
+      } else {
+        this.isLoggedIn = false;
+      }
+    },
   },
 };
 </script>
