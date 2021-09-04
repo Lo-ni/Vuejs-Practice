a4bc18924b36d0a6aab8069c4c797b2f242fe103
Loni <fisher-s@web.de>
2021-09-04 14:28:53 +0200

    added axios for network call and vue-formulate for forms

--- space-capsule-app/src/main.js
+++ space-capsule-app/src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import VueFormulate from "@braid/vue-formulate";
+import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueFormulate);
+Vue.use(axios);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
